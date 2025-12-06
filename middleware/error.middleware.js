198c16a044131922cb2e48e019b71cd510d2b56a
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-12-07 02:26:34 +0300

    implement error handling

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,29 +1,33 @@
 const errorMiddleware = (err, req, res, next) => {
-  const error = {...err}
-  
-  error.message = err.message
+  try {
+    const error = { ...err };
 
-  console.log(err)
+    error.message = err.message;
 
-  if (err.name === "CastError") {
-    const message = "Resource not found"
-    error = new Error(message)
-    error.statusCode = 404;
+    console.log(err);
+
+    if (err.name === "CastError") {
+      const message = "Resource not found";
+      error = new Error(message);
+      error.statusCode = 404;
+    }
+
+    if (err.code === 11000) {
+      const message = "duplicated field value error";
+      error = new Error(message);
+      error.statusCode = 400;
+    }
+
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      error = new Error(messages.join(", "));
+      error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).json({ success: false, error: error.message || "server error" });
+  } catch (error) {
+    next(error);
   }
+};
 
-  if (err.code === 11000) {
-    const message = 'duplicated field value error'
-    error = new Error(message)
-    error.statusCode = 400
-  }
-
-  if (err.name === 'ValidationError') {
-    const messages = Object.values(err.errors).map(val => val.message )
-    error = new Error(messages.join(', '))
-    error.statusCode = 400
-  }
-
-  res.status(error.statusCode || 500).json({success: false, error: error.message || "server error"})
-}
-
-export default errorMiddleware
+export default errorMiddleware;
