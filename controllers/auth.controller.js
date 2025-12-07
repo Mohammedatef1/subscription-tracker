deb41c67a8c3a3b8d174326d63477a4adee49d6b
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-12-07 13:25:51 +0300

    implement sign in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -44,8 +44,40 @@ export const SignUp = async (req, res, next) => {
   }
 }
 
-export const SignIn = (req, res, next) => {
+export const SignIn = async (req, res, next) => {
+  try {
+      const {email, password} = req.body
 
+      const existingUser = await User.findOne({email})
+
+      if (!existingUser) {
+        const error = new Error('invalid credential')
+        error.statusCode = 401
+        throw error
+      }
+
+      const correctPassword = await bcrypt.compare(password, existingUser.password)
+
+      if (!correctPassword) {
+        const error = new Error('invalid credentials')
+        error.statusCode = 401
+        throw error
+      }
+
+      const token = jwt.sign({userId: existingUser._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN})
+
+      res.status(200).json({
+        success: true,
+        message: "singed in successfully",
+        data: {
+          token,
+          user: existingUser
+        }
+      })
+      
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const SignOut = (req, res, next) => {
